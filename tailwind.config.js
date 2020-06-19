d24ab8a9862542738be40c81024fe55d22a765b4
Hon Boey <hon@dialogicstudios.com.au>
2020-06-19 16:03:14 +1000

    Styled changed type to IBM Plex

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      "plex-sans": ["IBM Plex Sans", "sans-serif"],
+      "plex-serif": ["IBM Plex Serif", "serif"],
+      "plex-mono": ["IBM Plex Mono", "monospace"],
+    },
     extend: {
       colors: {
         "nd-magenta": "#d9318a",
