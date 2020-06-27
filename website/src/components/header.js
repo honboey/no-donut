9895f5eac9bf4ffb326ade4e793e54a37f4eed85
Hon Boey <hon@dialogicstudios.com.au>
2020-06-27 15:29:48 +1000

    Fixed summary to ABC link and added more detail in introduction

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -9,11 +9,13 @@ const Header = ({ siteTitle }) => (
         <img src={no} alt="no" className="lg:w-1/3" />
         <img src={donut} alt="donut" className="lg:w-2/3" />
     </h1>
-    <div className="flex justify-between flex-col lg:flex-row lg:flex-row-reverse mb-6">
+    <div className="flex justify-between flex-col lg:flex-row lg:flex-row-reverse mb-12">
       <div className="lg:w-2/3">
-        <p className="text-xl sm:text-2xl lg:text-4xl leading-tight mb-6">
+        <p className="text-xl sm:text-2xl lg:text-4xl leading-tight mb-6 mb-4 lg:mb-8">
           No Donut is a collection of resources building towards a copless
-          Australia
+          Australia.</p> 
+          
+          <p className="sm:text-lg lg:text-2xl leading-snug">This website is continually being updated and refined. If you have a resource you would like to share please email us at <a className="underline" href="mailto:badcopnodonut@dialogicstudios.com.au">badcopnodonut@dialogicstudios.com.au</a>.
         </p>
       </div>
       <div className="lg:w-1/3">
