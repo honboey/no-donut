1616cb093cbfb7b78313665542b2fd2b63c35d19
Hon Boey <hon@dialogicstudios.com.au>
2020-06-29 12:03:32 +1000

    Styled masthead for Safari

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -6,16 +6,30 @@ import donut from "../images/donut.gif"
 const Header = ({ siteTitle }) => (
   <header>
     <h1 className="flex mb-6 lg:mb-8 justify-between">
-        <img src={no} alt="no" className="lg:w-1/3" />
-        <img src={donut} alt="donut" className="lg:w-2/3" />
+      <div className="lg:w-1/3">
+        <img src={no} alt="no" className="w -full h-auto" />
+      </div>
+      <div className="lg:w-2/3">
+        <img src={donut} alt="donut" className="w-full h-auto" />
+      </div>
     </h1>
     <div className="flex justify-between flex-col lg:flex-row lg:flex-row-reverse mb-12 lg:mb-20">
       <div className="lg:w-2/3">
         <p className="text-xl sm:text-2xl lg:text-4xl leading-tight mb-6 mb-4 lg:mb-8">
           No Donut is a collection of resources building towards a copless
-          Australia.</p> 
-          
-          <p className="sm:text-lg lg:text-2xl leading-snug">This website is continually being updated and refined. If you have a resource you would like to share please email us at <a className="underline" href="mailto:badcopnodonut@dialogicstudios.com.au">badcopnodonut@dialogicstudios.com.au</a>.
+          Australia.
+        </p>
+
+        <p className="sm:text-lg lg:text-2xl leading-snug">
+          This website is continually being updated and refined. If you have a
+          resource you would like to share please email us at{" "}
+          <a
+            className="underline"
+            href="mailto:badcopnodonut@dialogicstudios.com.au"
+          >
+            badcopnodonut@dialogicstudios.com.au
+          </a>
+          .
         </p>
       </div>
       <div className="lg:w-1/3">
