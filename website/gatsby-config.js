628a5fc235ea2641e77bb7a320c5a4e7f0dcdc22
Hon Boey <hon@dialogicstudios.com.au>
2020-07-06 15:45:18 +1000

    Added favicon

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -35,9 +35,9 @@ module.exports = {
           {
             baseId: process.env.AIRTABLE_BASE,
             tableName: process.env.AIRTABLE_TABLE_NAME,
-          }
-        ]
-      }
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -48,7 +48,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-postcss`,
